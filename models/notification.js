abb7fa52bd63e435dd4b7f681f069c30b05b5ce2
yuanzm <1229084233@qq.com>
2015-06-12 13:06:41 +0800

    add message and its unit test

--- models/notification.js
+++ models/notification.js
@@ -1,19 +1,20 @@
+// 引入所需模块
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 var BaseModel = require("./base_model");
 
-var NotificationScema = new Schema({
-    type: {type: String},
-    master_id: {type: ObjectId},
-    author_id: {type: ObjectId},
-    topic_id: {type: ObjectId},
-    comment_id: {type: ObjectId},
-    has_read: {type: Boolean, default: false},
-    create_at: {type: Date, default: Date.now}
+var NotificationSchema = new Schema({
+    type: {type: String},			// 消息的类型
+    sender_id: {type: ObjectId},	// 发送者的id
+    receiver_id: {type: ObjectId},	// 接受者的id
+    topic_id: {type: ObjectId},		//	与消息有关的话题
+    comment_id: {type: ObjectId},	// 与消息有关的评论
+    has_read: {type: Boolean, default: false},	// 消息是否已读
+    create_at: {type: Date, default: Date.now}	// 创建时间
 });
 
-NotificationScema.plugin(BaseModel);
-NotificationScema.index({master_id: 1, has_read: -1, create_at: -1});
+NotificationSchema.plugin(BaseModel);
+NotificationSchema.index({master_id: 1, has_read: -1, create_at: -1});
 
-mongoose.model('Notification', NotificationScema);
+mongoose.model('Notification', NotificationSchema);
